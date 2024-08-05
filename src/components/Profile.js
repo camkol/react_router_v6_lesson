f531bd993442a1cf6c2b992f536aafbea4ab8c74
camkol <koliyus3@hotmail.com>
2024-08-05 12:11:15 +0200

    provide a router,basic routing with <Route>,link to routes,dynamic routes,useParams,nest routes,

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-import { selectCurrentUser, selectIsLoggedIn } from "../features/session/sessionSlice";
+import { Link, Outlet } from "react-router-dom";
+import {
+  selectCurrentUser,
+  selectIsLoggedIn,
+} from "../features/session/sessionSlice";
 
-export default function Profile () {
-  const currentUser = useSelector(selectCurrentUser)
+export default function Profile() {
+  const currentUser = useSelector(selectCurrentUser);
   const loggedIn = useSelector(selectIsLoggedIn);
-  
+
   // use loggedIn to return a Navigate
 
   return (
@@ -14,6 +17,7 @@ export default function Profile () {
       <h1>{currentUser.username}</h1>
       <Link to={`edit`}>Edit</Link>
       {/* Tell React Router where to render child routes` */}
+      <Outlet />
     </main>
-  )
+  );
 }
